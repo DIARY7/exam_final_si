1e27548ac38f43dc74039a276f5e39ed9eff7f96
DIARY7 <diaryhakeur@gmail.com>
2024-07-15 15:47:00 +0300

    update sql

--- script.sql
+++ script.sql
@@ -15,42 +15,43 @@ CREATE TABLE garage_type_service(
 	PRIMARY KEY(id)
 );
 
-CREATE TABLE garage_type_voiture(
+CREATE TABLE garage_type_vehicule(
 	id int auto_increment,
 	designation varchar(50),
 	PRIMARY KEY(id)
 );
 
-CREATE TABLE garage_vehicul(
+CREATE TABLE garage_vehicule(
 	numero int,
 	id_type int,
 	PRIMARY KEY(numero),
-	FOREIGN KEY (id_type) references garage_type_voiture(id)
+	FOREIGN KEY (id_type) references garage_type_vehicule(id)
 );
 CREATE TABLE garage_rdv(
 	id int auto_increment,
 	date_rdv datetime,
 	id_type_service int,
-	id_voiture int,
+	id_vehicule int,
 	id_slot int,
-	dateFin date,
+	date_fin date,
 	PRIMARY KEY(id),
 	FOREIGN KEY (id_type_service) references garage_type_service(id),
-	FOREIGN KEY (id_voiture) references garage_vehicul(numero),
+	FOREIGN KEY (id_vehicule) references garage_vehicule(numero),
 	FOREIGN KEY (id_slot) references garage_slot(id)
 );
 
-CREATE OR REPLACE VIEW vehicul_view AS
-SELECT l.*,t.designation AS designation FROM garage_vehicul l JOIN garage_type_voiture t on l.id_type = t.designation;
+CREATE OR REPLACE VIEW garage_vehicule_view AS
+SELECT l.*,t.designation AS designation FROM garage_vehicule l JOIN garage_type_vehicule t on l.id_type = t.designation;
 
-CREATE OR REPLACE VIEW rdv_view AS
+CREATE OR REPLACE VIEW garage_rdv_view AS
 SELECT r.*, ts.designation,ts.duree,ts.montant,s.designation as slot FROM garage_rdv r JOIN garage_type_service ts ON r.id_type_service = ts.id
-JOIN vehicul_view l ON r.id_voiture = l.numero
+JOIN garage_vehicule_view l ON r.id_vehicule = l.numero
 JOIN garage_slot s ON r.id_slot = s.id;  
 
 
 INSERT INTO garage_slot (designation) VALUES ('A');
 INSERT INTO garage_slot (designation) VALUES ('B');
+INSERT INTO garage_slot (designation) VALUES ('C');
 
 INSERT INTO garage_type_service (id, designation, duree, montant) VALUES 
 (1, 'Réparation simple', '01:00:00', 150000.00),
